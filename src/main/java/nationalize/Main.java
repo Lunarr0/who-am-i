72b7d047ea34da872bfdfbf15b8bf8464752493f
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-01-08 08:17:11 +0100

    fixed styles issue and depencies

--- src/main/java/nationalize/Main.java
+++ src/main/java/nationalize/Main.java
@@ -10,4 +10,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @Controller
-public 
+public class Main {
+    private final NationalizeClient nationalizeClient;
+
+    public Main() {
+        this.nationalizeClient = NationalizeClient.newInstance();
+    }
+
+    @GetMapping("/")
+    public String showForm() {
+        return "index";
+    }
+
+    @PostMapping("/nationalize")
+    public String analyzeNationality(@RequestParam("name") String name, Model model) {
+        Nationality nationality = nationalizeClient.getNationality(name);
+        model.addAttribute("nationality", nationality);
+        model.addAttribute("name", name);
+        return "result";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
