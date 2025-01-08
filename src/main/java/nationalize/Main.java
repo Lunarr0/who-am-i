665c3a583d8d987a8e9f62596cebdb6a5c7c1a47
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-01-08 07:38:05 +0100

    Update files, styles and functionality and README.md

--- src/main/java/nationalize/Main.java
+++ src/main/java/nationalize/Main.java
@@ -10,25 +10,4 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @Controller
-public class Main {
-
-    @GetMapping("/")
-    public String showForm() {
-        return "index";
-    }
-
-    @PostMapping("/nationalize")
-    public String getNationality(@RequestParam String name, Model model) {
-        var client = NationalizeClient.newInstance();
-        var nationality = client.getNationality(name);
-        
-        model.addAttribute("name", name);
-        model.addAttribute("nationality", nationality);
-        
-        return "result";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
-}
+public 
