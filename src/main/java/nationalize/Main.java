997a97d731d2edf7ffcaa1b6a5d5fc58d9d4b1d1
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-01-08 06:42:15 +0100

    added springboot to application

--- src/main/java/nationalize/Main.java
+++ src/main/java/nationalize/Main.java
@@ -1,18 +1,25 @@
 package nationalize;
 
-/**
- * CLI for the {@code nationalize} package.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
 public class Main {
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.printf("Usage: java %s <name>%n", Main.class.getName());
-            System.exit(1);
-        }
+    @PostMapping("/nationalize")
+    @ResponseBody
+    public String getNationality(@RequestParam String name) {
         var client = NationalizeClient.newInstance();
-        var nationality = client.getNationality(args[0]);
-        System.out.println(nationality);
+        var nationality = client.getNationality(name);
+        return nationality.toString();
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
